dde6c47714860f87b9438f4ceb20715b6769d614
“JaneChengYang” <you@example.com>
2018-03-28 20:50:45 +0800

    Initial Commit

---
+++ uniqlo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uniqlo
+//
+//  Created by Simon on 2018/3/28.
+//  Copyright © 2018年 Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
